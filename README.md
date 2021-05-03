3c34d5c8551f83c61556062d62c552a9fe75e878
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-03 10:15:07 +0000

    Project organization updates

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# Textual Data Labeler interface for accessor
+# Data Labeling interface
+WIP: Does not launch
+Still scaffolding
 
-
-## Инструкция по запуску
-1. Убедитесь, что у вас установлен питон версии 3
-2. Опционально создайте и активируйте виртуальное окружение
-3. Установите зависимости из файла requirements.txt
-4. Выполните `python3 app.py` из директории проекта. Страница будет находиться по адресу http://127.0.0.1:5000/
 
--- app/db/storage.py
+++ app/db/storage.py
@@ -1,4 +1,21 @@
-from ABC import metaclass
+from abc import ABCmeta
 
-class IStorage(metaclass=):
+class IUserStorage(metaclass=ABCmeta):
+
     
+class IDataSetStorage(metaclass=ABCmeta):
+    '''
+    /-- meta
+    dataset name
+    uploaded date
+    last updated
+    /-- data labeling
+    target_field
+    target_field_type
+    label_field
+    label_field_type
+    user_based_labeling
+    /-- data cleaning
+    allow_modify
+    allow_upsert    
+    '''
