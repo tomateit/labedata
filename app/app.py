bb8f04e77e0ab2872697b6d7311f86f067912902
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-03 10:14:49 +0000

    Routes drafts

--- app/app.py
+++ app/app.py
@@ -1,32 +1,57 @@
 import os
 import config
+import datetime
+import pathlib
 
-app = Flask(__name__)
-app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+from flask import Flask, render_template, request
+from flask import abort, redirect, url_for, session
 
 from . import views
 
-from flask import Flask
-from flask import Flask, render_template, request
-import pandas as pd
-import datetime
-import csv
-import os
-import pathlib
-
-
 app = Flask(__name__)
+app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' #! replace
+
 
 launch_time = datetime.datetime.now().isoformat()
-fout_ = open(f"./output/train_data_results.csv", "w")
-csv_writer = csv.writer(fout_)
-
-data = pd.read_csv("./data_chunk_2020-12-09T14:49:08.148942.csv")
-data = data[["label", "text"]].to_dict("records")
-
 
 @app.route("/", methods=["GET", "POST"])
 def index_page(result=""):
+    if 'username' in session:
+        return show_index_page()
+    else:
+        return redirect_to_login_form()
+
+from flask import make_response
+@app.route("/login", methods=["GET", "POST"])
+def login_page(result=""):
+    error = None
+    if request.method == 'POST':
+        username, password = request.form['username'], request.form['password']
+        error = validate_credentials(username, password)
+        if error is None:
+            session['username'] = username
+            # resp = make_response(render_template("index.html"))
+            # resp.set_cookie('username', 'the username')
+            return redirect(url_for('index_page'))
+            return resp
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+from werkzeug.utils import secure_filename
+@app.route('/dataset/new', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['dataset']
+        f.save('../input/'+ secure_filename(f.filename))
+    else:
+        return render_template('dataset_new.html', error=error)
+
+
+@app.route("/dataset/<dataset>/<entity>", methods=["GET", "POST"])
+def index_page(dataset, entity):
 
     if request.method == "POST":
         result = request.form["result"]
@@ -38,12 +63,12 @@ def index_page(result=""):
         print("First run")
 
     if not len(data):
-        return render_template("index.html", 
+        return render_template("dataset_entity.html", 
             data="Большое спасибо за труд! На сегодня всё."
         )
     else :
         d = data[0]["text"]
-        return render_template("index.html", data=d, count_left=str(len(data)))
+        return render_template("dataset_entity.html", data=d, count_left=str(len(data)))
 
 
 
