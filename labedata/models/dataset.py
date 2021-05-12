98318107b79f8ce9417c54b7578d221d75474e9e
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-12 16:34:56 +0000

    Dataset class now can be created

--- labedata/models/dataset.py
+++ labedata/models/dataset.py
@@ -4,6 +4,7 @@ import pandas as pd
 import uuid 
 from slugify import slugify
 from csv_dataset import CSVDataset
+from typing import List, Union
 
 TYPE_TO_CLASS = {"csv": CSVDataset}
 
@@ -25,17 +26,15 @@ class DatasetFactory(metaclass=ABCmeta):
     allow_upsert    
     '''
     @staticmethod
-    def new(self, dataset_id=None, data=None):
-        assert (data is not None) or (dataset_id is not None), "Either data or dataset_id must be provided"
-        if data is None:
-            data = db.execute(
-                f"SELECT *\
-                FROM datasets WHERE dataset_id = ${dataset_id}"
-            ).fetchone()
-        return TYPE_TO_CLASS[data["dataset_format"]]
-
-        for key, value in data.items():
-            self[key] = value
+    def fetch_by_id(self, dataset_id)-> Union[Dataset, None]:
+        assert dataset_id is not None, "Must provide dataset_id"
+        db = get_db()
+        dataset_meta = db.execute(
+            f"SELECT *\
+            FROM datasets WHERE dataset_id = ${dataset_id}"
+        ).fetchone()
+        CLASS = TYPE_TO_CLASS[data["dataset_format"]]
+        return CLASS(**data)
 
     @staticmethod
     def user_id_to_colname(self, user_id) -> str:
@@ -43,7 +42,7 @@ class DatasetFactory(metaclass=ABCmeta):
         return user_id + "_" + self.slug
 
     @staticmethod
-    def fetch_by_author_id(author_id):
+    def fetch_by_author_id(author_id) -> List:
         db = get_db()
         return db.execute(
             f"SELECT title, created_at, username\
@@ -54,14 +53,15 @@ class DatasetFactory(metaclass=ABCmeta):
     @staticmethod
     def create(data) -> Dataset:
         #! 1. validate request meta
-        meta_fields = ["input_path", "dataset_format"]
+        meta_fields = [] # ??
         #! 2. validate dataset fields
         dataset_fields = ["title", 
             "author_id", 
             "data_field", 
             "data_field_type", 
             "label_field", 
-            "label_field_type", 
+            "label_field_type",
+            "input_path", "dataset_format" ,
             "user_based_labeling",
             "allow_modify_data",
             "allow_upsert_data",
@@ -75,15 +75,35 @@ class DatasetFactory(metaclass=ABCmeta):
         data["slug_id"] = slug + dataset_id.replace("-", "_")
 
         #! 3. process dataset input file and save to output_path
-        output_path = DATASET_CLASS.perform_file_processing(data)
+        data["output_path"] = DATASET_CLASS.perform_file_processing(data)
         
-            
         #! 4. create new entity
-        
+        db = get_db()
         #! return Dataset instance
+        return DATASET_CLASS(**data)
         
 
 class Dataset(DatasetFactory, metaclass=ABCmeta):
+    def __init__(self, **data):
+        self.dataset_id = data["dataset_id"]
+        self.title  = data["title "]
+        self.slug = data["slug"]
+        self.slug_id = data["slug_id"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        self.author_id  = data["author_id "]
+        self.data_field  = data["data_field "]
+        self.data_field_type  = data["data_field_type "]
+        self.label_field  = data["label_field "]
+        self.label_field_type = data["label_field_type"]
+        self.input_path  = data["input_path "]
+        self.output_path = data["output_path"]
+        self.dataset_format = data["dataset_format"]
+        self.user_based_labeling = data["user_based_labeling"]
+        self.allow_modify_data = data["allow_modify_data"]
+        self.allow_upsert_data = data["allow_upsert_data"]
+        self.allow_delete_data = data["allow_delete_data"]
+
     @abstractmethod
     @staticmethod
     def perform_file_processing(data) -> str:
