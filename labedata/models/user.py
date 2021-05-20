9f9f4d92ed7826478275bc8dd498a784e5fae806
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-20 17:46:19 +0000

    Linting and minor chages

--- labedata/models/user.py
+++ labedata/models/user.py
@@ -1,26 +1,26 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from ..db import get_db
-from typing import Union, Dict, Tuple
+from typing import Union, Dict, Tuple, Optional
 from uuid import uuid4
 
 class User():
 
     @staticmethod
-    def fetch_by_login(login) -> Union[Dict[str, str], None]:
+    def fetch_by_login(login) -> Optional[Dict[str, str]]:
         db = get_db()
         return db.execute(
             "SELECT * FROM users WHERE login = ?", (login,)
         ).fetchone()
 
     @staticmethod
-    def fetch_by_user_id(user_id) -> Union[Dict[str, str], None]:
+    def fetch_by_user_id(user_id) -> Optional[Dict[str, str]]:
         db = get_db()
         return db.execute(
             "SELECT * FROM users WHERE user_id = ?", (user_id,)
         ).fetchone()
 
     @staticmethod
-    def register_new_user(username=None, password=None, login=None, email=None, **kwargs) -> str:
+    def register_new_user(username=None, password=None, login=None, email=None, **kwargs) -> Optional[str]:
         error = None
 
         # server-side validation even though form has it"s own
@@ -35,16 +35,17 @@ class User():
             error = "Login is required."
         elif User.fetch_by_login(login) is not None:
             error = f"Login {login} is already registered."
-        user_id = str(uuid4())
-        db = get_db()
-        db.execute("INSERT INTO users (user_id, login, username, email, password) VALUES (?, ?, ?, ?, ?)",
-                    (user_id, login, username, email, generate_password_hash(password))
-        )
-        db.commit()
+        else:
+            user_id = str(uuid4())
+            db = get_db()
+            db.execute("INSERT INTO users (user_id, login, username, email, password) VALUES (?, ?, ?, ?, ?)",
+                        (user_id, login, username, email, generate_password_hash(password))
+            )
+            db.commit()
         return error
 
     @staticmethod
-    def authenticate(login: str=None, password: str=None)-> Tuple[Union[None, Dict[str,str]], Union[str, None]]:
+    def authenticate(login: str=None, password: str=None)-> Tuple[Optional[Dict[str,str]], Optional[str]]:
         error = None
         if not login:
             return None, "Login not provided"
