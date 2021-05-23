99057fc7e52560b27c035e14dd48bfcd943ecdd4
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-23 17:58:58 +0000

    Dataset Entity now can be shown

--- labedata/dataset.py
+++ labedata/dataset.py
@@ -30,7 +30,7 @@ def dataset_new():
         #TODO proper form validation
         converted_form = { key: form[key].data for key in incoming_dataset_fields}
         ds = Dataset.create(**converted_form, input_path=filename, author_id=g.user["user_id"])
-        return redirect(url_for(f"dataset/{ds.dataset_id}"))
+        return redirect(url_for(f"dataset.dataset", dataset_id=ds.dataset_id))
     else:
         return render_template("dataset_new.html", form=form)
 
@@ -40,7 +40,6 @@ def dataset(dataset_id):
     #TODO! Only author can access and modify dataset meta
     ds = Dataset.fetch_by_id(dataset_id)
     print(f"REquested dataset {dataset_id}, got {ds}")
-    print(bool(ds))
     if not ds:
         return redirect(url_for("index"), code=404)
     if request.method == "GET":
@@ -57,35 +56,44 @@ def dataset(dataset_id):
 @bp.route("/<string:dataset_id>/next", methods=["GET"])
 @login_required
 def next_entity(dataset_id):
-    next_entity = Dataset.fetch_by_id(dataset_id).next_entity_for_user_id(g.user["user_id"])
-    return redirect(entity_page, dataset=dataset, entity=next_entity)
+    next_entity_id = Dataset.fetch_by_id(dataset_id).next_entity_for_user_id(g.user["user_id"])
+    if next_entity_id:
+        return redirect(url_for("dataset.entity_page", dataset=dataset_id, entity=next_entity_id))
+    else:
+        print("No more entitiles left")
+        return redirect(url_for("index"))
 
 
-@bp.route("/<string:dataset>/<uuid:entity>", methods=["GET", "POST", "PATCH", "PUT", "DELETE"])
+@bp.route("/<string:dataset_id>/<string:entity>", methods=["GET", "POST", "PATCH", "PUT", "DELETE"])
 @login_required
-def entity_page(dataset, entity):
+def entity_page(dataset_id, entity):
+    ds = Dataset.fetch_by_id(dataset_id)
+    print(f"REquested dataset {dataset_id}, got {ds}")
+    if not ds:
+        print(f"Dataset {dataset_id} not found")
+        return redirect(url_for("index"), code=404)
     #!TODO validate that user was assigned to this dataset
     # GET and PUT do not redirect, show requested (possibly modified) entity again
     # POST (UPSERT) redirects to newly created entity
     # DELETE and PATCH redirect to NEXT
     if request.method == "GET":
-        entity = Dataset.fetch_by_id(dataset).get_entity(entity)
-        return render_template("dataset_entity", entity=entity)
+        entity = ds.get_entity(entity)
+        return render_template("dataset_entity.html", entity=entity, dataset=ds)
 
     if request.method == "POST":
-        entity = Dataset.fetch_by_id(dataset).upsert_entity(request.form)
-        return render_template("dataset_entity", entity=entity)
+        entity = ds.upsert_entity(request.form)
+        return render_template("dataset_entity.html", entity=entity, dataset=ds)
     
     # main labeling action
     if request.method == "PATCH":
-        Dataset.fetch_by_id(dataset).label_entity(entity, label, user_id=g.user["user_id"])
+        ds.label_entity(entity, label, user_id=g.user["user_id"])
         return redirect(url_for("next_entity"))
 
     if request.method == "PUT":
-        entity = Dataset.fetch_by_id(dataset).modify_entity(entity, request.form)
-        return render_template("dataset_entity", entity=entity)
+        entity = ds.modify_entity(entity, request.form)
+        return render_template("dataset_entity.html", entity=entity, dataset=ds)
 
     if request.method == "DELETE":
-        Dataset(dataset).delete_entity(entity)
+        ds.delete_entity(entity)
         return redirect(url_for("next_entity"))
 
--- labedata/models/_csv_dataset.py
+++ labedata/models/_csv_dataset.py
@@ -17,6 +17,7 @@ class CSVDataset(Dataset):
         dataframe.rename(columns = {data["data_field"]: "data_field", data["label_field"]: "label_field"}, inplace = True)
  
         dataframe.drop_duplicates(subset=["data_field"], inplace=True)
+        dataframe = dataframe.assign(label_field=pd.NA)
         #!TODO ensure data_field and label_field fit *_field_type
 
         dataframe["entity_id"] = [str(uuid.uuid4()) for _ in range(len(dataframe))]
@@ -30,38 +31,39 @@ class CSVDataset(Dataset):
         #TODO line-wise CSV reading
         location = Path(current_app.config["OUTPUT_DIR"], f"{self.slug_id}.csv")
         try:
+            print(f"Opening dataset at {location}")
             dataset = pd.read_csv(location, index_col="entity_id")
             yield dataset
         finally:
             #? wut
+            print(f"Saving dataset to {location}")
             dataset.to_csv(location)
             # update updated_at in dataset meta
 
     def next_entity_for_user_id(self, user_id):
         with self.__get_dataset() as dataset:
             if self.user_based_labeling:
+                print("Requested next entity for dataset w/per-user labeling")
                 column_name = self.user_id_to_colname(user_id)
                 # 1. ensure dataset is ready for user-based labeling
                 # i.e. it has column for the user_id
                 if column_name not in dataset.cols:
                     dataset[column_name] = pd.NA
                 try:
-                    return dataset[dataset[column_name].isna()].iloc[0].index
+                    return dataset[dataset[column_name].isna()].head(1).index.item()
                 except IndexError:
                     return None
             else:
+                print("Requested next entity for commonly-labeled dataset")
                 try:
-                    return dataset[dataset[label_field].isna()].iloc[0].index
+                    index = dataset[dataset["label_field"].isna()].head(1).index.item()
+                    return index
                 except IndexError:
                     return None
 
     def get_entity(self, entity_id) -> Dict[str, Any]:
         with self.__get_dataset() as dataset:
-            if self.user_based_labeling:
-                column_name = self.user_id_to_colname(user_id)
-                dataset.at[entity_id, column_name] = label
-            else:
-                dataset.at[entity_id, "label_field"]
+            return {"data_field": dataset.loc[entity_id, "data_field"]}
 
     def label_entity(self, entity_id, label) -> None:
         with self.__get_dataset() as dataset:
